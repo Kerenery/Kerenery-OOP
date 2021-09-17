1a3e167e92f30aece30429d609a53b14c7b3ee7d
Nick <djhitekdieant@gmail.com>
2021-09-17 16:45:47 +0300

    fix: implement + test namings

--- Isu.Tests/IsuServiceTest.cs
+++ Isu.Tests/IsuServiceTest.cs
@@ -14,18 +14,18 @@ namespace Isu.Tests
         public void Setup()
         {
             //TODO: implement
-            _isuService = null;
+            _isuService = new IsuService();
         }
 
         [Test]
-        public void AddStudentToGroup_StudentHasGroupAndGroupContainsStudent()
+        public void AddStudentToGroup_StudentHasGroupAndGroupContainsStudent_ThrowException()
         {
             Assert.Catch<IsuException>(() =>
             {
-                var isu = new IsuService();
-                isu.AddGroup("M3206");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick");
+                var group = new Group("M3206");
+                _isuService.AddGroup("M3206");
+                _isuService.AddStudent(group, "Nick");
+                _isuService.AddStudent(group, "Nick");
             });
         }
 
@@ -34,37 +34,14 @@ namespace Isu.Tests
         {
             Assert.Catch<IsuException>(() =>
             {
-                var isu = new IsuService();
-                isu.AddGroup("M3206");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick2");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick3");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick4");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick5");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick6");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick7");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick8");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick9");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick10");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick11");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick12");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick13");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick14");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick15");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick16");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick17");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick18");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick19");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick20");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick21");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick22");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick23");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick24");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick25");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick26");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick27");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick28");
-                isu.AddStudent(isu.FindGroup("M3206"), "Nick29");
+                var group = new Group("M3206");
+                _isuService.AddGroup("M3206");
+                int count = default;
+                
+                for (int i = 0; i < 30; ++i)
+                {
+                    var student = _isuService.AddStudent(group, $"NICK POOPICH{++count}");
+                }
             });
         }
 
@@ -72,23 +49,21 @@ namespace Isu.Tests
         public void CreateGroupWithInvalidName_ThrowException()
         {
             Assert.Catch<IsuException>(() =>
-            {
-                var isu = new IsuService();
-                isu.AddGroup("Invalid format");
+            { 
+                _isuService.AddGroup("M1488");
             });
         }
 
         [Test]
-        public void TransferStudentToAnotherGroup_GroupChanged()
+        public void TransferStudentToAnotherGroup_InvalidGroup_ThrowException()
         {
             Assert.Catch<IsuException>(() =>
             {
-                var isu = new IsuService();
-                isu.AddGroup("M3211");
-                isu.AddGroup("M3206");
-                Group superGroup = new Group("M3230");
-                isu.AddStudent(isu.FindGroup("M3211"), "Nickolasha");
-                isu.ChangeStudentGroup(isu.FindStudent("Nickolasha"),superGroup);
+                _isuService.AddGroup("M3211");
+                _isuService.AddGroup("M3206");
+                var superGroup = new Group("M3230");
+                _isuService.AddStudent(_isuService.FindGroup("M3211"), "Nickolasha");
+                _isuService.ChangeStudentGroup(_isuService.FindStudent("Nickolasha"),superGroup);
             });
         }
     }
