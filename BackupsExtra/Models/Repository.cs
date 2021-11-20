8b88df8ee60644a617c3991e87aafe20f1c17a2f
Nick <djhitekdieant@gmail.com>
2021-11-20 18:22:30 +0300

    feat: repository iequatable

--- BackupsExtra/Models/Repository.cs
+++ BackupsExtra/Models/Repository.cs
@@ -1,7 +1,11 @@
-﻿namespace BackupsExtra.Models
+﻿using System;
+
+namespace BackupsExtra.Models
 {
-    public class Repository
+    public class Repository : IEquatable<Repository>
     {
         public string Path { get; init; }
+        public bool Equals(Repository other)
+            => other != null && Path == other.Path;
     }
 }
