d0da160232ee5b522d1017eca28527e4b1595977
Nick <djhitekdieant@gmail.com>
2021-11-20 18:21:54 +0300

    feat: added backupJob process

--- BackupsExtra/Services/BackupExtraService.cs
+++ BackupsExtra/Services/BackupExtraService.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using BackupsExtra.Models;
+using BackupsExtra.Tools;
+using Serilog;
 
 namespace BackupsExtra.Services
 {
@@ -11,7 +15,32 @@ namespace BackupsExtra.Services
 
         public BackupJob AddBackupJob(BackupJob backupJob)
         {
-            if(_backups.Any(b => b.Repository == backupJob.))
+            if (backupJob is null)
+            {
+                Log.Error("backupJob is null");
+                throw new BackupsExtraException("backupJob can't be null");
+            }
+
+            if (!Directory.Exists(backupJob.Destination.Path))
+                Log.Warning("directory does not exist");
+
+            if (_backups.All(b => b.Repository != backupJob.Destination))
+            {
+                var backup = new Backup()
+                {
+                    Repository = backupJob.Destination,
+                    Id = Guid.NewGuid(),
+                };
+
+                _backups.Add(backup);
+                Log.Information($"{backup.Id} backup is created");
+            }
+
+            _backupJobs.Add(backupJob);
+            var selectedBackup = _backups.First(b => b.Repository == backupJob.Destination);
+            selectedBackup.AddRestorePoint(backupJob.Algorithm.Copy(backupJob.JobObject, backupJob.Destination, selectedBackup.Term));
+            Log.Information("backupJob process finished, created new restore point");
+            return backupJob;
         }
     }
 }
