3be71a6a280f4abcad0edab1befd475029347e29
Nick <djhitekdieant@gmail.com>
2021-11-21 13:23:20 +0300

    fix: override equals

--- BackupsExtra/Models/Repository.cs
+++ BackupsExtra/Models/Repository.cs
@@ -7,5 +7,15 @@ namespace BackupsExtra.Models
         public string Path { get; init; }
         public bool Equals(Repository other)
             => other != null && Path == other.Path;
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            return obj is Repository repository && Equals(repository);
+        }
+
+        public override int GetHashCode() => base.GetHashCode();
     }
 }
